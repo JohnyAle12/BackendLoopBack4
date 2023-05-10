3b796951cdaa75dd989209e553867dd1dbdbe269
Johny Prieto <johnyprieto001@gmail.com>
2023-05-09 21:55:27 -0500

    create category and product models, and its repositories

---
+++ src/models/category.model.ts
@@ -0,0 +1,37 @@
+import {Entity, hasMany, model, property} from '@loopback/repository';
+import {Product} from './product.model';
+
+@model()
+export class Category extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @hasMany(() => Product)
+  products?: Product[];
+
+  constructor(data?: Partial<Category>) {
+    super(data);
+  }
+}
+
+export interface CategoryRelations {
+  // describe navigational properties here
+}
+
+export type CategoryWithRelations = Category & CategoryRelations;
---
+++ src/models/product.model.ts
@@ -0,0 +1,48 @@
+import {Entity, belongsTo, model, property} from '@loopback/repository';
+import {Category} from './category.model';
+
+@model()
+export class Product extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  @property({
+    type: 'string',
+  })
+  image?: string;
+
+  @belongsTo(() => Category)
+  categoryId: number;
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
